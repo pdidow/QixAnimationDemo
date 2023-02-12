29db8334a4d2ce17b2b18a81b82b75fddd11028a
Peter Didow <pdidow@gmail.com>
2023-02-12 11:19:09 -0700

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace QixAnimationDemo
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        float t;
+        static readonly int NUM_LINES = 5;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _graphics.IsFullScreen = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            t += 0.05f;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            for (int i = 0; i < NUM_LINES; i++)
+            {
+                Vector2 start = new Vector2(x1(t), y1(t));
+                Vector2 end = new Vector2(x2(t), y2(t));
+                DrawLine(start, end, Color.White, 5);
+                t += 0.05f;
+            }
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+        public void DrawLine(Vector2 start, Vector2 end, Color color, float thickness = 1)
+        {
+            var distance = Vector2.Distance(start, end);
+            var angle = (float)Math.Atan2(end.Y - start.Y, end.X - start.X);
+            Texture2D lineTexturePoint;
+            lineTexturePoint = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+            lineTexturePoint.SetData(new Color[] { Color.Red });
+            _spriteBatch.Draw(lineTexturePoint, start, null, color, angle, Vector2.Zero, new Vector2(distance, thickness), SpriteEffects.None, 0);
+        }
+        float x1(float t)
+        {
+            return (float)(Math.Sin(t / 10) * 100 + Math.Sin(t / 5) * 20);
+        }
+
+        float y1(float t)
+        {
+            return (float)(Math.Cos(t / 10) * 100);
+        }
+
+        float x2(float t)
+        {
+            return (float)(Math.Sin(t / 10) * 200 + Math.Sin(t) * 2);
+        }
+
+        float y2(float t)
+        {
+            return (float)(Math.Cos(t / 20) * 200 + Math.Cos(t / 12) * 20);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new QixAnimationDemo.Game1();
+game.Run();
