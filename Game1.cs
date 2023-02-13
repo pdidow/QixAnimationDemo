90330e2384957967c97c477ff14938f01bd5a291
Peter Didow <pdidow@gmail.com>
2023-02-12 17:53:04 -0700

    Changed start and end Vector Declarations as they were incorrect.

--- Game1.cs
+++ Game1.cs
@@ -17,6 +17,8 @@ namespace QixAnimationDemo
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = 500;
+            _graphics.PreferredBackBufferHeight = 500;
             _graphics.IsFullScreen = true;
         }
 
@@ -51,11 +53,12 @@ namespace QixAnimationDemo
             _spriteBatch.Begin();
             for (int i = 0; i < NUM_LINES; i++)
             {
-                Vector2 start = new Vector2(x1(t), y1(t));
-                Vector2 end = new Vector2(x2(t), y2(t));
+                Vector2 start = new Vector2(x1(t+i), y1(t+i));
+                Vector2 end = new Vector2(x2(t+i), y2(t+i));
                 DrawLine(start, end, Color.White, 5);
-                t += 0.05f;
+                
             }
+            t += 0.05f;
             _spriteBatch.End();
 
             base.Draw(gameTime);
@@ -69,24 +72,34 @@ namespace QixAnimationDemo
             lineTexturePoint.SetData(new Color[] { Color.Red });
             _spriteBatch.Draw(lineTexturePoint, start, null, color, angle, Vector2.Zero, new Vector2(distance, thickness), SpriteEffects.None, 0);
         }
+        //public void DrawLineWithFade(Vector2 start, Vector2 end, Color color, float thickness = 1)
+        //{
+        //    color.A = (byte)(255 / NUM_LINES * (NUM_LINES - thickness));
+        //    var distance = Vector2.Distance(start, end);
+        //    var angle = (float)Math.Atan2(end.Y - start.Y, end.X - start.X);
+        //    Texture2D lineTexturePoint;
+        //    lineTexturePoint = new Texture2D(_graphics.GraphicsDevice, 1, 1);
+        //    lineTexturePoint.SetData(new Color[] { Color.Red });
+        //    _spriteBatch.Draw(lineTexturePoint, start, null, color, angle, Vector2.Zero, new Vector2(distance, thickness), SpriteEffects.None, 0);
+        //}
         float x1(float t)
         {
-            return (float)(Math.Sin(t / 10) * 100 + Math.Sin(t / 5) * 20);
+            return (float)(Math.Sin(t / 10) * 100 + Math.Sin(t / 5) * 20 + 200);
         }
 
         float y1(float t)
         {
-            return (float)(Math.Cos(t / 10) * 100);
+            return (float)(Math.Cos(t / 10) * 100 + 200);
         }
 
         float x2(float t)
         {
-            return (float)(Math.Sin(t / 10) * 200 + Math.Sin(t) * 2);
+            return (float)(Math.Sin(t / 10) * 200 + Math.Sin(t) * 2 + 200);
         }
 
         float y2(float t)
         {
-            return (float)(Math.Cos(t / 20) * 200 + Math.Cos(t / 12) * 20);
+            return (float)(Math.Cos(t / 20) * 200 + Math.Cos(t / 12) * 20 + 200);
         }
     }
 }
